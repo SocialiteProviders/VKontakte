7ad0c4f861b1b98c0e20c717d1f9506fa7dea574
DraperStudio <hello@draperstud.io>
2015-03-07 12:01:47 +0100

    Update comments

--- src/VKontakteExtendSocialite.php
+++ src/VKontakteExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class VKontakteExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
