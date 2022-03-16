2ef1f4e0ef0564832462e3635a261dbeb7c58ba6
Aaron Christiansen <42357661+clink-aaron@users.noreply.github.com>
2022-03-16 04:06:10 -0600

    okta revoke and introspect functionality (#816)

---
+++ Provider.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace SocialiteProviders\VKontakte;
+
+use Illuminate\Support\Arr;
+use Laravel\Socialite\Two\InvalidStateException;
+use RuntimeException;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    protected $fields = ['id', 'email', 'first_name', 'last_name', 'screen_name', 'photo_200'];
+
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'VKONTAKTE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $stateless = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['email'];
+
+    /**
+     * Last API version.
+     */
+    public const VERSION = '5.131';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://oauth.vk.com/authorize',
+            $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://oauth.vk.com/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $formToken = [];
+
+        if (is_array($token)) {
+            $formToken['email'] = $token['email'] ?? null;
+
+            $token = $token['access_token'];
+        }
+
+        $params = http_build_query([
+            'access_token' => $token,
+            'fields'       => implode(',', $this->fields),
+            'lang'         => $this->getConfig('lang', 'en'),
+            'v'            => self::VERSION,
+        ]);
+
+        $response = $this->getHttpClient()->get('https://api.vk.com/method/users.get?'.$params);
+
+        $contents = (string) $response->getBody();
+
+        $response = json_decode($contents, true);
+
+        if (!is_array($response) || !isset($response['response'][0])) {
+            throw new RuntimeException(sprintf(
+                'Invalid JSON response from VK: %s',
+                $contents
+            ));
+        }
+
+        return array_merge($formToken, $response['response'][0]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        if ($this->hasInvalidState()) {
+            throw new InvalidStateException();
+        }
+
+        $response = $this->getAccessTokenResponse($this->getCode());
+
+        $user = $this->mapUserToObject($this->getUserByToken($response));
+
+        $this->credentialsResponseBody = $response;
+
+        if ($user instanceof User) {
+            $user->setAccessTokenResponseBody($this->credentialsResponseBody);
+        }
+
+        return $user->setToken($this->parseAccessToken($response))
+            ->setExpiresIn($this->parseExpiresIn($response));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => Arr::get($user, 'id'),
+            'nickname' => Arr::get($user, 'screen_name'),
+            'name'     => trim(Arr::get($user, 'first_name').' '.Arr::get($user, 'last_name')),
+            'email'    => Arr::get($user, 'email'),
+            'avatar'   => Arr::get($user, 'photo_200'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * Set the user fields to request from Vkontakte.
+     *
+     * @param array $fields
+     *
+     * @return $this
+     */
+    public function fields(array $fields)
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['lang'];
+    }
+}
---
+++ VKontakteExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SocialiteProviders\VKontakte;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class VKontakteExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('vkontakte', Provider::class);
+    }
+}
